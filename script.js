17cc258ba16cc54569b13b971baeb16f048ce989
itsbbea <bea.may27@hotmail.com>
2023-10-30 03:29:39 +1100

    updated script.js file with added function to generate SVG logo with various shapes, installed git ignore and package.json

--- script.js
+++ script.js
@@ -7,4 +7,21 @@ class Shape {
     }
 }
 
+addShape(Shape); {
+    this.shapes.push(Shape);
+}
 
+generateSVG(); {
+    let svg = `<svg width="${this.width}" height="${this.height}" xmlns="http://www.w3.org/2000/svg">`;
+
+    for (const shape of this.shapes) {
+        if (shape.type === "circle") {
+            svg += `<circle cx="${shape.x}" cy="${shape.y}" r="50" fill="${shape.color}" />`;
+        } else if (shape.type === "rectangle") {
+            svg += `<rect x="${shape.x}" y="${shape.y}" width="100" height="50" fill="${shape.color}" />`;
+        }
+    }
+
+    svg += `</svg>`;
+    return svg;
+}
